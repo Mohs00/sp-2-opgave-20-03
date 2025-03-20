d7d7039e881eb45ad806179b38a67d0098174ca9
Mohs00 <moemen122003@gmail.com>
2025-03-20 19:35:37 +0100

    Add files via upload

---
+++ LibraryRoyaltyCalculator.java
@@ -0,0 +1,22 @@
+public class LibraryRoyaltyCalculator {
+    public static void main(String[] args) {
+
+        Author author = new Author("Olga Ravn");
+
+
+        author.addTitle(new PrintedBook("TE", 140, 72));
+        author.addTitle(new PrintedBook("SKØN", 50, 300));
+        author.addTitle(new AudioBook("SKØN", 10, 400) {
+            @Override
+            public double calculatePoints() {
+                return 0;
+            }
+        });
+
+
+        double totalPay = author.calculateTotalPay();
+
+
+        System.out.printf("%s: %.2fkr%n", author.getName(), totalPay);
+    }
+}
